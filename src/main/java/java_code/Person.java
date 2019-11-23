d2eb36dfe630554a5bd523834f472db3022745de
erhan-d <dolapcierhan@hotmail.com>
2019-11-23 10:38:42 -0500

    version2.1

--- src/main/java/java_code/Person.java
+++ src/main/java/java_code/Person.java
@@ -2,8 +2,12 @@ package java_code;
 
 public class Person {
     private int age;
-    private String firstName;
-    private String lastName;
+    static String firstName="Holy";
+    static String lastName="Moly";
+
+    public static void main(String[] args) {
+        System.out.println(firstName +" " +lastName );
+    }
 
 }
 
---
+++ src/main/java/java_code/Person2.java
@@ -0,0 +1,8 @@
+package java_code;
+
+public class Person2 {
+    public static void main(String[] args) {
+        System.out.println("Second person");
+
+    }
+}
